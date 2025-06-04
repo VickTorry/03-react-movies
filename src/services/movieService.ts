bf82148d4e31b0cf1ab69a3bbcae1b2149d4d3e5
VickTorry <victoriadudnyk@gmail.com>
2025-06-04 20:22:40 +0200

    created serch-film webcite

---
+++ src/services/movieService.ts
@@ -0,0 +1,18 @@
+// src/services/movieService.ts
+import axios from 'axios';
+import type { Movie } from '../types/movie';
+
+const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+interface fetchMoviesResponse {
+  results: Movie[];
+}
+
+export default function fetchMovies(query: string) {
+  return axios.get<fetchMoviesResponse>('https://api.themoviedb.org/3/search/movie', {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+    params: { query },
+  });
+};
